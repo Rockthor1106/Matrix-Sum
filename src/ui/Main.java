ab34833d166f17040111d08423f1a5c5fc964ba1
jhan carlos carvajal bastidas <liceo.sur.andino2015@gmail.com>
2021-09-30 23:36:50 -0500

    I created two matrices, A and B and a loop to sum all their elements and
    save them in a new matrix called C

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,6 +3,32 @@ package ui;
 public class Main {
 
 	public static void main(String[] args) {
+		int[][] A = new int[2][2];
+		
+		A[0][0] = 2;
+		A[0][1] = 3;
+		A[1][0] = 4;
+		A[1][1] = 5;
+		
+		int[][] B = new int[2][2];
+		
+		B[0][0] = 25;
+		B[0][1] = 34;
+		B[1][0] = 43;
+		B[1][1] = 52;
+		
+		int[][] C = new int[2][2];
+		
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A.length; j++) {
+				C[i][j] = A[i][j]+B[i][j];
+			}
+		}
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A.length; j++) {
+				System.out.println(C[i][j]);
+			}
+		}
 
 	}
 
